b06934f7a65c920cd63d5a68013ab3b70b17d8ef
Khaing Khant <khaingkhantjob@gmail.com>
2023-05-14 10:13:03 +0630

    deploy test to vercel

--- components/navbar.js
+++ components/navbar.js
@@ -1,19 +1,19 @@
-import Logo from "./logo";
-import NextLink from 'next/link'
+// import Logo from "./logo";
+// import NextLink from 'next/link'
 
-import {
-    Container,
-    Box,
-    Link,
-    Stack,
-    Heading,
-    Flex,
-    Menu,
-    MenuItem,
-    MenuList,
-    MenuButton,
-    IconButton,
-    useColorModeValue
-} from '@chakra-ui/react';
+// import {
+//     Container,
+//     Box,
+//     Link,
+//     Stack,
+//     Heading,
+//     Flex,
+//     Menu,
+//     MenuItem,
+//     MenuList,
+//     MenuButton,
+//     IconButton,
+//     useColorModeValue
+// } from '@chakra-ui/react';
 
-import { HamburgerIcon } from '@chakra-ui/icon'
+// import { HamburgerIcon } from '@chakra-ui/icon'
